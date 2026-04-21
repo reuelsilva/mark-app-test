e8da117d359caefc28455ad83fbe2a963f766402
reuelsilva <reuelsilva19@gmail.com>
2026-04-21 13:15:36 -0300

    fix: define modo headless com base no ambiente CI no Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: !!process.env.CI,
     launchOptions: {
       slowMo: 300,
     },
